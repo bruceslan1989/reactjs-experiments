6f1f6fb443fa5f2679b764259728bebd1ba8c14c
Ruslans Tarasovs <ruslans.tarasovs@ms-idi.eu>
2017-08-28 22:01:00 +0300

    Added creating.

---
+++ src/components/AddProject.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+    static defaultProps = {
+        categories: [
+            'Category 1',
+            'Category 2',
+            'Category 3',
+        ]
+    };
+
+    constructor() {
+        super();
+        this.state = {
+            project: {
+
+            }
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        if (this.refs.title.value.trim() === '') {
+            alert("Title is required!");
+            return;
+        }
+
+        console.log(
+            this.refs.title.value,
+            this.refs.category.value
+        );
+
+        this.setState({project: {
+            title: this.refs.title.value,
+            category: this.refs.category.value
+        }}, function() {
+            this.props.creatingCallback(this.state.project);
+            this.refs.title.value = null;
+            this.refs.category.value = 'Category 1';
+        });
+    }
+
+    render() {
+        let categoryOptions = this.props.categories.map(category => {
+            return <option key={category} value={category}>{category}</option>
+        });
+        return (
+            <div>
+                <h3>Add Project</h3>
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <div>
+                        <label>Title</label><br />
+                        <input type="text" ref="title"/>
+                    </div>
+                    <div>
+                        <label>Category</label><br />
+                        <select ref="category">
+                            {categoryOptions}
+                        </select>
+                    </div>
+                    <input type="submit" value="Add" />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AddProject;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 import Project from "./Project";
+import AddProject from "./AddProject";
 
 class Projects extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            projects: []
+        }
+    }
+
+    componentWillMount() {
+        this.setState({
+            projects: [{
+                title: "Title 1",
+                category: "Category 1"
+            }, {
+                title: "Title 2",
+                category: "Category 2"
+            }, {
+                title: "Title 3",
+                category: "Category 3"
+            }]
+        });
+    }
+
+    handleCreating(project) {
+        let projects = this.state.projects;
+        projects.push(project);
+        this.setState({
+            projects: projects
+        })
+    }
     render() {
         let projects;
-        if (this.props.projects) {
-            projects = this.props.projects.map(project => {
-                return (
-                    <Project key={project.title} project={project} />
-                );
-            });
-        }
+        projects = this.state.projects.map(project => {
+            return (
+                <Project key={project.title} project={project} />
+            );
+        });
         return (
             <div>
                 <h1>Projects</h1>
+                <AddProject creatingCallback={this.handleCreating.bind(this)}/>
                 <div className="projects">
                     {projects}
                 </div>
